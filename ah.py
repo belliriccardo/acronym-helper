60b797fbb5a8cd240d7a31c4c8e45d8d74e4509d
Riccardo Belli <belliricc@gmail.com>
2023-05-06 20:48:19 +0200

    Mostly basic linting

--- ah.py
+++ ah.py
@@ -1,43 +1,51 @@
+""" :file:`ah.py` file """
 from difflib import SequenceMatcher
 from functools import lru_cache
 import argparse
 import random
-import click
 import csv
 import os
+import click
 
 
 @lru_cache(maxsize=None)
 def similar(s_1: str, s_2: str) -> float:
+    """ :func:`similar` function """
     # Misura di somiglianza tra due stringhe; memoizzato in quanto può essere oneroso
     return SequenceMatcher(None, s_1, s_2).ratio()
 
 
-def isValidAsciiChar(c: int) -> bool:
+def is_valid_ascii_char(char: int) -> bool:
+    """ :func:`is_valid_ascii_char` function """
     # Lettere e numeri più spazio
-    return 65 <= c <= 90 or 97 <= c <= 122 or 48 <= c <= 57 or c == 32
+    return 65 <= char <= 90 or 97 <= char <= 122 or 48 <= char <= 57 or char == 32
 
 
-def clearscreen(no_scroll: bool = False) -> None:
+def clear_screen(no_scroll: bool = False) -> None:
+    """ :func:`clear_screen` function """
     if no_scroll:
         os.system('cls' if os.name == 'nt' else 'clear')
     else:
         print("\033[2J\033[1;1H", end='', flush=True)
 
 
-def loadAcronymsFromFile(file_path: str, uniqueness_delimiter: str) -> dict:
+def load_acronyms_from_file(file_path: str, uniqueness_delimiter: str) -> dict:
+    """ :func:`load_acronyms_from_file` function """
     acronyms = dict()
-    with open(file_path) as csv_file:
+    with open(file_path, "rt", encoding='utf-8') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for (acronym, extended, description) in csv_reader:
-            acronym += uniqueness_delimiter + f'{random.randint(0, 1_000_000_000):09d}'
-            acronyms[acronym] = (extended, description.replace(r'\n', '\n').replace(r'\t', '\t'))
+            acronym += uniqueness_delimiter + \
+                f'{random.randint(0, 1_000_000_000):09d}'
+            acronyms[acronym] = (extended, description.replace(
+                r'\n', '\n').replace(r'\t', '\t'))
 
     return acronyms
 
 
 def main(args) -> None:
-    clearscreen(args.noscroll)
+    """ :func:`main` function """
+    clear_screen(args.noscroll)
 
     file_name = args.file
     show_closest_n = args.show
@@ -46,7 +54,7 @@ def main(args) -> None:
 
     delimiter = '_'
 
-    acronyms = loadAcronymsFromFile(file_name, delimiter)
+    acronyms = load_acronyms_from_file(file_name, delimiter)
 
     while True:
 
@@ -65,7 +73,7 @@ def main(args) -> None:
 
             if k_ord == 27:  # esc
                 raise KeyboardInterrupt()
-            elif isValidAsciiChar(k_ord) or k_ord == 127:
+            if is_valid_ascii_char(k_ord) or k_ord == 127:
                 if k_ord == 127:  # backspace
                     typed = typed[:-1]
                 else:
@@ -101,7 +109,7 @@ def main(args) -> None:
 
                     text_extra += '\n'
 
-        clearscreen(args.noscroll)
+        clear_screen(args.noscroll)
 
         click.secho(text, fg=args.col, nl=False)
         click.echo(text_extra, nl=False)
@@ -120,19 +128,19 @@ if __name__ == '__main__':
     parser.add_argument('--noscroll', required=False,
                         default=False, help='Pulire il terminale invece di farlo scorrere',
                         action='store_true')
-    args = parser.parse_args()
+    cli_args = parser.parse_args()
 
     try:
-        main(args)
+        main(cli_args)
     except KeyboardInterrupt:
         # Per aver premuto 'esc' o Ctrl-C; esco
-        clearscreen(args.noscroll)
+        clear_screen(cli_args.noscroll)
     except TypeError as e:
         # Carattere non ASCII o altra roba strana; per caso è attivo num lock?
         click.echo(f'Carattere inaspettato: {e}')
     except EOFError as e:
         # Probabilmente per aver premuto Ctrl-D per uscire dal terminale; esco
-        clearscreen(args.noscroll)
+        clear_screen(cli_args.noscroll)
     except Exception as e:
         # Ah boh finora mai successo però
         click.echo(f'Qualcos\'altro è andato storto! Verificare: {e}')
